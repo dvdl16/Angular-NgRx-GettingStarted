6f9ee9d3eca141150b391673f6e6ef99d34a02e2
Dirk van der Laarse <dirk.van_der_laarse@partner.bmw.co.za>
2022-05-08 15:37:33 +0200

    Add statically typed state and selectors for user module

--- APM/src/app/user/state/user.reducer.ts
+++ APM/src/app/user/state/user.reducer.ts
@@ -1,8 +1,24 @@
-import { createAction, on, createReducer } from "@ngrx/store";
+import { createAction, on, createReducer, createFeatureSelector, createSelector } from "@ngrx/store";
 
-export const userReducer = createReducer(
-    { maskUsername: false },
-    on(createAction('[User] Mask Username'), state => {
+export interface UserState {
+    maskUsername: boolean;
+}
+
+const initialState: UserState = {
+    maskUsername: false
+}
+
+// Selectors
+const getUserFeatureState = createFeatureSelector<UserState>('user');
+
+export const getMaskUsername = createSelector(
+    getUserFeatureState,
+    state => state.maskUsername
+)
+
+export const userReducer = createReducer<UserState>(
+    initialState,
+    on(createAction('[User] Mask Username'), (state): UserState => {
         return {
             ...state,
             maskUsername: !state.maskUsername
