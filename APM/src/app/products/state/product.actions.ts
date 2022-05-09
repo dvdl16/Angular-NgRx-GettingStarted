229c68bacf3319e4e250d7599b920cad4d06c713
Dirk van der Laarse <dirk.van_der_laarse@partner.bmw.co.za>
2022-05-09 10:05:03 +0200

    Add effects for product update action

--- APM/src/app/products/state/product.actions.ts
+++ APM/src/app/products/state/product.actions.ts
@@ -31,3 +31,18 @@ export const loadProductsFailure = createAction(
     '[Product] Load Failure',
     props<{ error: string }>()
 )
+
+export const updateProduct = createAction(
+    '[Product] Update Product',
+    props<{ product: Product }>()
+)
+
+export const updateProductSuccess = createAction(
+    '[Product] Update Product Success',
+    props<{ product: Product }>()
+)
+
+export const updateProductFailure = createAction(
+    '[Product] Update Product Failure',
+    props<{ error: string }>()
+)
